ef73b008bec07fb880daf996f231a14d3719b186
Jacky Liang <asheriffy@gmail.com>
2022-03-30 13:30:57 -0400

    commit tree 15a2e1728b726815644f9667f8cea63b2fe96c1a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+
+function submitData (name,email) {
+    const userData = {
+        name: "Steve",
+        email: "steve@steve.com",
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
+      
+      fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+        //  console.log(object.id);
+          h2 = document.createElement('h2')
+          h2.innerText = object.id
+          document.body.append(h2)
+        })
+
+
+
+        .catch(function () {
+        //   document.createElement('h1')
+        //   h1.innerText = "Bad things! Ragnarők!";
+        //   document.body.append(h1)
+          console.log('catch');
+        });
+}
